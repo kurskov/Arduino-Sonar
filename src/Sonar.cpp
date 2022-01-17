ff755de47d0eee25be3c114b84f6d3be53c8493e
Dmitrii Kurskov <dmitrii@kurskov.ru>
2022-01-17 16:22:46 +0300

    Version for tests

--- src/Sonar.cpp
+++ src/Sonar.cpp
@@ -9,41 +9,91 @@
 
 // Constructor
 
-Sonar::Sonar(uint8_t triggerPin, uint8_t echoPin , long maxTime) {
+Sonar::Sonar(uint8_t trigger_pin, uint8_t echo_pin , long max_time) {
     // set basic parameter
-    setTriggerPin(triggerPin);
-    setEchoPin(echoPin);
-    setMaxTime(maxTime);
-
-    pinMode(_triggerPin, OUTPUT);
-    if (!_onePin) {
-        pinMode(_echoPin, INPUT);
-    }
+    setTriggerPin(trigger_pin);
+    setEchoPin(echo_pin);
+    setMaxTime(max_time);
 }
 
+// Get functions
+
+long getTime(uint8_t iterations) {
+    unsigned long time = 0;
+    unsigned long single_time = 0;
+    uint8_t errors = 0;
+    
+    if ( !iterations ) { return 0; }
+
+    // main cycle
+    for (int i=0; i<iterations; i++) {
+        // for one signal pin
+        if ( _onePin ) {
+            pinMode(_triggerPin, OUTPUT);
+        }
+
+        // initialise of oscillator
+        digitalWrite(_triggerPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(_triggerPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(_triggerPin, LOW);
+        
+        // for one signal pin
+        if ( _onePin ) {
+            pinMode(_triggerPin, INPUT);
+        }
+        
+        single_time = pulseIn(_echoPin, HIGH, _maxTime);
+
+        if ( single_time )  {
+            time =+ single_time;
+        } else {
+            errors++;
+        }
+
+        delayMicroseconds(DELAY_BETWEEN_MEASURMENT);
+    }
+
+    // if all measurements are errors
+    if ( iterations == errors ) { return 0; }
+
+    // value (or middle value) in microseconds
+    return (long)(time / (iterations - errors)); 
+}
+
+float getDistance(uint8_t iterations,bool units) {
+    float distance = getTime(iterations) * SOUND_SPEED / 2000;
+    
+    if ( !units ) { return distance / 2.54; }
+
+    return distance;
+}
 
 // Set functions
 
-void Sonar::setTriggerPin(uint8_t triggerPin) {
-    _triggerPin = triggerPin;
+void Sonar::setTriggerPin(uint8_t trigger_pin) {
+    _trigger_pin = trigger_pin;
+    pinMode(_trigger_pin, OUTPUT);
 }
 
-void Sonar::setEchoPin(uint8_t echoPin) {
-    if (echoPin) {
-        _echoPin = echoPin;
+void Sonar::setEchoPin(uint8_t echo_pin) {
+    if (echo_pin && (echo_pin != _trigger_pin)) {
+        _echo_pin = echo_pin;
     } else {
-        _echoPin = _triggerPin;
-        _onePin = true;
+        _echo_pin = _trigger_pin;
+        _one_pin = true;
     }
+    pinMode(_echo_pin, INPUT);
 }
 
-void Sonar::setMaxTime(long maxTime) {
-    _maxTime = maxTime;
+void Sonar::setMaxTime(long max_time) {
+    _max_time = max_time;
 }
 
-void Sonar::setMaxDistance(int maxDistance, bool units) {
-    _maxTime = maxDistance * 29.1 * 2;
+void Sonar::setMaxDistance(int max_distance, bool units) {
+    _max_time = (long)(max_distance / SOUND_SPEED) * 2000;
     if (!units) {
-        _maxTime = _maxTime * 2.54;
+        _max_time = _max_time * 2.54;
     }_
 }
--- src/Sonar.h
+++ src/Sonar.h
@@ -6,7 +6,7 @@
     Author: Dmitrii Kurskov (dmitrii@kurskov.ru)
     GitHub: https://github.com/kurskov/Arduino-Sonar
     
-    *** Underconstruction ***
+    *** Underconstruction (for tests) ***
 */
 
 // Include guard
@@ -22,8 +22,9 @@
 //Default settings
 
 // Units 
-#define UNIT_CM true    // centimeters
-#define UNIT_IN false   // inches
+#define UNIT_CM true        // centimeters
+#define UNIT_IN false       // inches
+#define SOUND_SPEED 34.33   // m/s
 
 // Sensor parameters
 #define MAX_SENSOR_DISTANCE_CM 400 // in centimeters
@@ -32,18 +33,18 @@
 
 class Sonar {
     public:
-        Sonar(uint8_t triggerPin, uint8_t echoPin = 0, long maxTime = 38000);
-        long getTime();
-        int getDistance(bool units = UNIT_CM);
-        void setTriggerPin(uint8_t triggerPin);
-        void setEchoPin(uint8_t echoPin);
-        void setMaxTime(long maxTime);
-        void setMaxDistance(int maxDistance = MAX_SENSOR_DISTANCE_CM, bool units = UNIT_CM);
+        Sonar(uint8_t trigger_pin, uint8_t echo_pin = 0, long max_time = MAX_SENSOR_TIME_US);
+        long getTime(uint8_t iterations = 1);
+        float getDistance(uint8_t iterations = 1,bool units = UNIT_CM);
+        void setTriggerPin(uint8_t trigger_pin);
+        void setEchoPin(uint8_t echo_pin);
+        void setMaxTime(long max_time);
+        void setMaxDistance(int max_distance = MAX_SENSOR_DISTANCE_CM, bool units = UNIT_CM);
     private:
-        uint8_t _triggerPin;
-        uint8_t _echoPin;
-        long _maxTime;
-        bool onePin = false;
+        uint8_t _trigger_pin;
+        uint8_t _echo_pin;
+        long _max_time;
+        bool _one_pin = false;
 };
 
 #endif _H_7F8B8320_60C3_4750_ADDF_B3EB6F16676C
